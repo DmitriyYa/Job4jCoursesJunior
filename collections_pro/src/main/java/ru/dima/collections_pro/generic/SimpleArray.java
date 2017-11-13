73d30c1e4294e7eced53b978b9c69c1dbc7f4e4d
Dima <dmitriy.m.yakovlev@gmail.com>
2017-11-13 15:31:52 +0600

    5.2.1. Реализовать SimpleArray<T>

---
+++ collections_pro/src/main/java/ru/dima/collections_pro/generic/SimpleArray.java
@@ -0,0 +1,48 @@
+package ru.dima.collections_pro.generic;
+
+/**
+ * Доделать контейнер SimpleArray<T> добавить методы add, update, delete, get(int index);
+ *
+ * @param <T>
+ */
+public class SimpleArray<T> {
+    private Object[] objects;
+    private int index = 0;
+
+    /**
+     * @param size
+     */
+    public SimpleArray(int size) {
+        this.objects = new Object[size];
+    }
+
+    /**
+     * @param value
+     */
+    public void add(T value) {
+        this.objects[index++] = value;
+    }
+
+    /**
+     * @param position
+     * @return
+     */
+    public T get(int position) {
+        return (T) this.objects[position];
+    }
+
+    /**
+     * @param position
+     */
+    public void delete(int position) {
+        this.objects[position] = null;
+    }
+
+    /**
+     * @param position
+     * @param value
+     */
+    public void update(int position, int value) {
+        this.objects[position] = value;
+    }
+}
