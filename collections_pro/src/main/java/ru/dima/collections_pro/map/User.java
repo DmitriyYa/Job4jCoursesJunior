42d500e26bac61e7037b43ea4a91eed794a16505
Dima <dmitriy.m.yakovlev@gmail.com>
2017-11-25 13:45:19 +0600

    1. Создать модель User

--- collections_pro/src/main/java/ru/dima/collections_pro/map/User.java
+++ collections_pro/src/main/java/ru/dima/collections_pro/map/User.java
@@ -20,24 +20,4 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
-
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//
-//        User userOne = (User) o;
-//
-//        if (children != userOne.children) return false;
-//        if (name != null ? !name.equals(userOne.name) : userOne.name != null) return false;
-//        return birthday != null ? birthday.equals(userOne.birthday) : userOne.birthday == null;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        int result = name != null ? name.hashCode() : 0;
-//        result = 31 * result + children;
-//        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
-//        return result;
-//    }
 }
